8a46f7fe15a892b241bc7e6bd8cf15916f5abbcc
Hideki Matsui <bushidomatsui8@gmail.com>
2025-10-29 23:03:03 -0400

    acrescentando uma function

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
-const image = document.querySelector('.app__image');
+const imagem = document.querySelector('.app__image');
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    imagem.setAttribute('src', `/imagens/${contexto}.png`);
+}
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-    image.setAttribute('src', '/imagens/foco.png');
+    alterarContexto('foco');
 });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    image.setAttribute('src', '/imagens/descanso-curto.png');
+    alterarContexto('descanso-curto');
 });
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    image.setAttribute('src', '/imagens/descanso-longo.png');
+    alterarContexto('descanso-longo');
 });
-
-
 
